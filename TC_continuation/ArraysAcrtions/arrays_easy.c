a14972246964c646e9b91f1786038b0bc86086f0
decl1 <zengaming1122@mail.ru>
2022-09-29 08:53:52 +0300

    Покрасивше код

--- TC_continuation/ArraysAcrtions/arrays_easy.c
+++ TC_continuation/ArraysAcrtions/arrays_easy.c
@@ -8,17 +8,22 @@ void main() {
 	srand(time(0));
 	
 	int n = 0, modnum = 0, power = 0,temp = 0;
-	while (n > 1000 || n < 1) {
+
+	while (n > ARRLEN || n < 1) {
 		system("cls");
 		printf("Arr len (1-1000): ");
 		scanf_s(" %d", &n);
 	}
+
 	int arr[ARRLEN];
+
 	for (int i = 0; i < n; i++) {
 		arr[i] = -10 + (rand() % 21);
 	}
+
 	printf("Mod numb (arr[i)%modnumb == 0): ");
 	scanf_s(" %d", &modnum);
+
 	while (power < 2 || power > 5) {
 		system("cls");
 		printf("Power (2-5) (arr[i)%modnumb == 0)^power: ");
@@ -31,9 +36,11 @@ void main() {
 		}
 		
 	}
+
 	printf("\n");
 	printf("Delete all numb less than: ");
 	scanf_s(" %d", &temp);
+
 	for (int i = 0; i < n; i++) {
 		if (arr[i] < temp) {
 			for (int k = 0; k < n; k++) {
@@ -43,11 +50,13 @@ void main() {
 			i--;
 		}
 	}
+
 	for (int i = 0; i < n*0.5; i++) {
 		temp = arr[n-1 - i];
 		arr[n-1-i] = arr[i];
 		arr[i] = temp;
 	}
+
 	for (int i = 0; i < n; i++) {
 		printf("%d ", arr[i]);
 	}
